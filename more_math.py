cd143905c1804a655b1676d42f83673b5c2c77f7
Lazar Jovovic <lumix@Lazars-MacBook-Pro.local>
2025-09-21 21:55:56 +0200

    3.1

--- more_math.py
+++ more_math.py
@@ -1,13 +1,39 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: lj7829
+@ASSESSME.AUTHOR: Lazar Jovovic
 @ASSESSME.DESCRIPTION: Assignment 3.1
 @ASSESSME.ANALYZE: YES
 '''
 
+import math
+
+def fact(n):
+    if isinstance(n, int) and n > 0:
+        return math.factorial(n)
+    return 0
+
+def root(x):
+    if x > 0:
+        return math.sqrt(float(x))
+    return 0
+
+def trunk(a):
+    return math.trunc(float(a))
 
 def main():
-   pass
+   
+   val = input("Enter a numeric valure: ")
+   num = int(float(val))
+   print(f"{num} factorial = {fact(num)}")
+
+   val = input("Enter a numeric valure: ")
+   num = float(val)
+   print(f"The square root of {num} = {root(num)}")
+
+   val = input("Enter a numeric valure: ")
+   num = float(val)
+   print(f"{num} truncated = {trunk(num)}")
+
 
 
 if __name__ == "__main__":
